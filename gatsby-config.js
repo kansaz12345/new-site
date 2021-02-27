731d97a94dcdf9ef3dac79b448bba626992b9a6a
kansaz12345 <68417506+kansaz12345@users.noreply.github.com>
2021-02-27 13:34:57 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 With unemployment being on the decline in Germany and its job market
 being highly-regarded worldwide, many foreign internationals head towards
-Germany to look for a job. Finding a job and becoming a resident of
+Germany to look for a job. 
+Finding a job and becoming a resident of
 Germany is possible, however, there are certain procedures one must
 follow initially.
 If you are interested in finding work in Germany, you may go to Germany
